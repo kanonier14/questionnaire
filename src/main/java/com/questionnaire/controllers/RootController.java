67d254922e25720c7fd48cb52906d54b22305561
Igor <igorstrakhov@gmai.com>
2016-12-05 01:39:26 +0300

    add vk auth

--- src/main/java/com/questionnaire/controllers/RootController.java
+++ src/main/java/com/questionnaire/controllers/RootController.java
@@ -1,10 +1,24 @@
 package com.questionnaire.controllers;
 
+import com.questionnaire.core.Constants;
 import com.questionnaire.repository.QuestionnaireRepository;
+import com.vk.api.sdk.client.TransportClient;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.UserAuthResponse;
+import com.vk.api.sdk.objects.users.UserXtrCounters;
+import com.vk.api.sdk.queries.users.UsersNameCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author Igor_Strakhov
@@ -24,5 +38,23 @@ public class RootController {
     @RequestMapping(path = "/personalaccount")
     public String getPersonalAccountPage() {
         return "personalaccount";
+    }
+
+    @RequestMapping(path = "/authorization/user")
+    public String getAuthentication(HttpServletRequest request, HttpServletResponse response) throws ClientException, ApiException {
+        String code = request.getParameter("code");
+        TransportClient transportClient = HttpTransportClient.getInstance();
+        VkApiClient vk = new VkApiClient(transportClient);
+        UserAuthResponse authResponse = vk.oauth()
+                .userAuthorizationCodeFlow(Integer.valueOf(Constants.ID_APPLICATION), Constants.KEY_APPLICATION, "http://localhost:8080/authorization/user", code)
+                .execute();
+        UserActor actor = new UserActor(authResponse.getUserId(), authResponse.getAccessToken());
+        UserXtrCounters user = vk.users().get(actor).nameCase(UsersNameCase.NOMINATIVE).execute().get(0);
+        return "index";
+    }
+
+    @RequestMapping(path = "/authorize")
+    public void authorize(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.sendRedirect("https://oauth.vk.com/authorize?client_id=5761453&display=page&redirect_uri=http://localhost:8080/authorization/user&scope=friends&response_type=code&v=5.60");
     }
 }
---
+++ src/main/java/com/questionnaire/core/Constants.java
@@ -0,0 +1,10 @@
+package com.questionnaire.core;
+
+/**
+ * Created by Igor on 04.12.2016.
+ */
+public interface Constants {
+
+    public static final String ID_APPLICATION = "5761453";
+    public static final String KEY_APPLICATION = "AxIEFTA6l1gQ4etsnsRJ";
+}
